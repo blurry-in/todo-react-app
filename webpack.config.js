a43b3a884adfa4c2740443a9cfb23311bee4e7cc
Karan Rawal <kr14nov@outlook.com>
2017-12-11 22:59:30 +0530

    Integrated the webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const Path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
 
 const SRC_DIR = Path.resolve(__dirname, 'src');
 const DIST_DIR = Path.resolve(__dirname, 'dist');
@@ -15,7 +16,14 @@ const config = {
       template: `${SRC_DIR}/index.html`,
       filename: `${DIST_DIR}/index.html`,
     }),
+    new Webpack.HotModuleReplacementPlugin(),
   ],
+  devServer: {
+    hot: true,
+    open: true,
+    inline: true,
+    contentBase: '/',
+  },
 };
 
 module.exports = config;
